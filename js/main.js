11f8278e96952dff0e75a5a3f47fe49a8974c70c
Cory Carpenter <corycarpenter@SARAHs-MacBook-Air-2.local>
2014-06-13 14:35:12 -0700

    Consolidate search

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
 /* Open and close main menu */
 
-$("#hamburger").click(function() {
-  $("#menu-overlay").fadeIn("fast");
-});
+$(document).ready(function() {
 
-$(".close-main-menu").click(function() {
-  $("#menu-overlay").fadeOut("fast");
+  $("#hamburger").click(function() {
+    $("#menu-overlay").fadeIn("fast");
+  });
+
+  $(".close-main-menu").click(function() {
+    $("#menu-overlay").fadeOut("fast");
+  });
+
+  $("#search-placeholder").click(function() {
+    $(this).hide();
+    $(".search-focus").show();
+    $(".search-focus").focus();
+  });
+
 });
